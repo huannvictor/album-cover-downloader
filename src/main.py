8b8c74ee9ce145dbf277e45ad405da8a2ff51e97
Huann Almeida <huannvictor@gmail.com>
2026-09-11 00:04:44 -0300

    Implementa a funcionalidade de download da capa do álbum com busca por artista e tratamento de erros. Remove código obsoleto.

---
+++ src/main.py
@@ -0,0 +1,49 @@
+import urllib.request
+import urllib.parse
+import json
+import os
+
+def main():
+
+    try:
+        album = input("Insira o nome do album: ")
+        artst = input("De qual artista/banda: ")
+
+        searchTerm = urllib.parse.quote(f'{album} {artst}')
+        
+        url = f"https://itunes.apple.com/search?term={searchTerm}&media=music&entity=song"
+
+        img_url = None
+
+        with urllib.request.urlopen(url) as response:
+            data = json.load(response)
+
+            for item in data.get('results', []):
+                if (
+                    item.get('artistName', '').strip().lower() == artst.strip().lower()
+                    and item.get("collectionName", "").strip().lower() == album.strip().lower()
+                ):
+                    
+                    img_url = item.get("artworkUrl100")
+
+                    if img_url:
+                        img_url = img_url.rsplit('/', 1)[0] + '/1000x1000.jpg'
+                        print(f'{img_url}')
+
+                        file_name = f'{album.replace(' ', '_')}.jpg'
+
+                        urllib.request.urlretrieve(img_url, file_name)
+
+                        os.startfile(file_name)
+
+                        break
+
+            if not img_url:
+                print('Nenhua capa encontrada correspondente.')
+
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')
+
+
+if __name__ == "__main__":
+    main()
